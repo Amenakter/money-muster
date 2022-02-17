de1d9cbf956b51ded7e3e2bb8cd821f1f1de2297
Amenakter <amenakter27@gmail.com>
2022-02-17 18:01:49 +0600

    function implementing complete

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+
+// calculate button
 document.getElementById('cal-btn').addEventListener('click', function () {
     getInputValue()
 })
 
-document.getElementById('save-btn').addEventListener('click', function () {
-    // console.log('save btn is click');
-    getSaveAmount();
-
-    // console.log(saveInput);
-})
 function getInputValue() {
     const foodInput = document.getElementById('food-input').value;
     const rantInput = document.getElementById('rant-input').value;
@@ -17,29 +13,38 @@ function getInputValue() {
     parseFloat(rantInput) +
     parseFloat(clothesInput);
     
-//  total expenses  
+// total expenses  
   const expenses = document.getElementById('expenses');
   expenses.innerText = totalExpInputValue
 
 //total balance
  updatebalance('total',totalExpInputValue )
 }
- 
-function updatebalance(id,totalExpInputValue) {
-    const balance = document.getElementById(id + '-balance');
-    const totalIncome = document.getElementById('income-input').value;
-    balance.innerText = parseFloat(totalIncome) - totalExpInputValue;
-}
+
+
+// save button
+document.getElementById('save-btn').addEventListener('click', function () {
+    // console.log('save btn is click');
+    getSaveAmount();
+})
+
 function getSaveAmount() {
     const saveInput = document.getElementById('save-input').value;
-    const totalIncome = document.getElementById('income-input').value;
+    const totalIncome = document.getElementById('totalIncome').value;
     const totalSaved = parseFloat(totalIncome) / 100 * parseFloat(saveInput);
-
+    
+    //saving Amount 
     const saveAmount = document.getElementById('save-amount')
     saveAmount.innerText = totalSaved;
-    console.log(totalSaved)
 
     // remainig balance
-    updatebalance('remaing',totalSaved)
+    const remaingBalance = document.getElementById('remaing-balance')
+    const totalBalance = document.getElementById('total-balance').innerText
+    remaingBalance.innerText = parseFloat(totalBalance) - totalSaved
 
+}
+function updatebalance(id,totalExpInputValue) {
+    const balance = document.getElementById(id + '-balance');
+    const totalIncome = document.getElementById(id +'Income').value;
+    balance.innerText = parseFloat(totalIncome) - totalExpInputValue;
 }
